3f43e5a11412ed69b42e7e476634fac90961e90a
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-10-28 15:23:17 -0500

    Fix test collection

--- test_minimize_water.py
+++ test_minimize_water.py
@@ -1,9 +1,11 @@
 from openff.interchange._tests import MoleculeWithConformer, ForceField
+
 import openmm
 
-simulation = ForceField("tip4p_fb.offxml").create_interchange(
-    MoleculeWithConformer.from_smiles("O").to_topology(),
-).to_openmm_simulation(integrator=openmm.VerletIntegrator(2.0))
-
-simulation.context.computeVirtualSites()
-simulation.minimizeEnergy()
+def test_minimize_basic():
+    simulation = ForceField("tip4p_fb.offxml").create_interchange(
+        MoleculeWithConformer.from_smiles("O").to_topology(),
+    ).to_openmm_simulation(integrator=openmm.VerletIntegrator(2.0))
+ 
+    simulation.context.computeVirtualSites()
+    simulation.minimizeEnergy()
